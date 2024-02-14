02f2567f2d787617dcaca9f66f45c116dcede339
jmejias08 <josuemejiasvillalobos@gmail.com>
2024-02-14 11:03:54 -0600

    workshop 2 fixed bugs

--- workshop2/client/index.js
+++ workshop2/client/index.js
@@ -1,81 +1,85 @@
-
-// Función que se ejecuta cuando la petición AJAX para el tipo de cambio se completa
-const completed = (response) => {
-    const data = JSON.parse(response.target.responseText);
-    document.getElementById('result').innerHTML = `Tipo de Cambio Dolares ${data.TipoCompraDolares}`;
-};
-
-// Función que maneja errores en la petición AJAX
+// Esta función maneja los errores que pueden ocurrir durante la solicitud de información.
 const error = () => console.log(this.responseText);
 
 
-  
-  // Función para realizar la petición AJAX al hacer clic en el botón
-function getExchange() {
-    const ajaxRequest = new XMLHttpRequest();
-    ajaxRequest.addEventListener("load", completed);
-    ajaxRequest.addEventListener("error", error);
-    ajaxRequest.open("GET", "http://localhost:3001/tipocambio");
-    ajaxRequest.send();
-}
-
-
-
-function sendCurrency() {
-    var comboBox = document.getElementById("countries");
-    var selection = comboBox.options[comboBox.selectedIndex].value;
-
-    // Crear una instancia de XMLHttpRequest o utilizar Fetch API
-    var xhr = new XMLHttpRequest();
-
-    // Configurar la solicitud POST al servidor
-    xhr.open("POST", "http://localhost:3001/paises", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    // Enviar los datos al servidor como JSON
-    xhr.send(JSON.stringify({ selection: selection }));
-
-
-    getCurrencyValue();
-
-}
-
-  
-
-// Otra petición AJAX para obtener información sobre todos los países
+// Esta funcion toma los datos del api de paises y los carga en combo box
 const ajaxRequestCountries = new XMLHttpRequest();
 ajaxRequestCountries.addEventListener("load", (e) => {
-    const countries = JSON.parse(e.target.responseText);
-    let optionsHtml = "";
-    countries.forEach(country => {
-        optionsHtml += `<option value="${country.currency}">${country.name}</option>`;
-    });
-    document.getElementById("countries").innerHTML = optionsHtml;
+  const countries = JSON.parse(e.target.responseText);
+  let optionsHtml = "";
+  countries.forEach(country => {
+      optionsHtml += `<option value="${country.currency}">${country.name}</option>`;
+  });
+  document.getElementById("countries").innerHTML = optionsHtml;
 });
 ajaxRequestCountries.addEventListener("error", () => {});
 ajaxRequestCountries.open("GET", "http://localhost:3001/paises");
 ajaxRequestCountries.send();
 
 
-function getCurrencyValue() {
-    const ajaxRequestResult = new XMLHttpRequest();
-    ajaxRequestResult.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const data = JSON.parse(this.responseText);
-  
-          const dolarValue = data.dolarValue;
-          const eurValue = data.eurValue;
-          console.log('dolar: ', dolarValue);
-  
-          document.getElementById('result').innerHTML = `
-            <p>Dólar: ${dolarValue}</p>
-            <p>Euro: ${eurValue}</p>
-          `; 
-        } else if (this.readyState === 4 && this.status !== 200) {
-          console.error('Error al obtener datos. Código de estado:', this.status);
-        }
-      };
-  
-      ajaxRequestResult.open('GET', 'http://localhost:3001/result', true);
-      ajaxRequestResult.send();
+// Esta función se encarga de enviar información sobre el país seleccionado
+// ademas, al finaliza llama la funcion de obtner los datos del tipo de cambio.
+async function sendCurrency() {
+  // se obtiene el pais seleccionado
+  const comboBox = document.getElementById("countries");
+  const selection = comboBox.options[comboBox.selectedIndex].value;
+
+  try {
+      // Se solicita al servidor informacion deln pais seleccionado
+      const response = await fetch("http://localhost:3001/paises", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ selection: selection }),
+      });
+
+      // En caso de error
+      if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      
+      const responseData = await response.json(); // respuesta es en formato JSON.
+      console.log("Response data:", responseData);
+
+      
+  } catch (error) {
+      console.error("Error sending currency:", error);
+  }
+  // se llama a la funcion de obtener el tipo de cambio  
+  getCurrencyValue();
 }
+
+
+async function getCurrencyValue() {
+  try {
+    // solicitud de datos utilizando fetch.
+    const response = await fetch('http://localhost:3001/result');
+
+
+    if (response.ok) {
+      // Se convierten los datos en json
+      const data = await response.json();
+
+      // se obtiene los valores de dólar y euro.
+      const dolarValue = data.dolarValue;
+      const eurValue = data.eurValue;
+      console.log('Dólar:', dolarValue);
+
+      // se imprimen los datos de los valores
+      document.getElementById('result').innerHTML = `
+          <p>Dólar: ${dolarValue}</p>
+          <p>Euro: ${eurValue}</p>
+      `;
+    } else {
+      console.error('Error al obtener datos. Código de estado:', response.status);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+
+
+
+
+
--- workshop2/server/index.js
+++ workshop2/server/index.js
@@ -1,12 +1,11 @@
-const axios = require('axios');
-
 const express = require('express');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-let dolarValue ;
+// Variables globales con el valor del dolar y el euro
+let dolarValue ; 
 let eurValue ;
 
 
@@ -22,40 +21,35 @@ app.use(cors({
 // routes
 // Endpoint /paises
 app.get('/paises', (req, res) => {
-  // Listado de países con la información solicitada
   const paises = [
     { name: "Costa Rica", currency: "crc" },
     { name: "Nicaragua", currency: "nio" },
     { name: "Mexico", currency: "mxn" },
     { name: "Panama", currency: "pab" },
-    { name: "Colombia", currency: "cop" }
+    { name: "Colombia", currency: "cop" },
+    { name: "Estados Unidos", currency: "usd"},
+    { name: "España", currency: "eur"}
   ];
   // Responde con el listado de países en formato JSON
   res.json(paises);
-
 });
-
 
 
 // Ruta para manejar las solicitudes POST en /paises
 app.post('/paises', async (req, res) => {
   const currency = req.body.selection;
   console.log("Currency selected:", currency);
-
   try {
-    const response = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`);
-
-    dolarValue = response.data[currency].usd;
-    eurValue = response.data[currency].eur;
-
+    const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`);
+    const responseData = await response.json();
+    dolarValue = responseData[currency].usd;
+    eurValue = responseData[currency].eur;
     console.log("dolar: ", dolarValue, " euro: ", eurValue);
 
-    // Enviar una respuesta al cliente si es necesario
     res.send('Datos recibidos en el servidor.');
 
-    // Desencadenar la ejecución de la ruta GET /result después del procesamiento del POST /paises
-    triggerResultRoute();
-
+    resResultCurrency();
+    
   } catch (error) {
     console.error("Error en la solicitud GET /paises:", error);
     res.status(500).send('Error en el servidor.');
@@ -64,21 +58,16 @@ app.post('/paises', async (req, res) => {
 
 
 // Función para manejar la ejecución de la ruta GET /result
-const triggerResultRoute = () => {
+const resResultCurrency = () => {
   app.get('/result', (req, res) => {
     console.log("dolarV: ", dolarValue)
     const request = {
       dolarValue: dolarValue,
-      eurValue: eurValue
+      eurValue: eurValue,
     }
     res.json(request);
   });
 };
 
- 
 
-
-
-
-//start the app
 app.listen(3001, () => console.log(`BBCR Exchange type service listening on port 3001!`))
